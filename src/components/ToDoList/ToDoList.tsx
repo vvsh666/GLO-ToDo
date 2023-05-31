c1538e99995e8f74924889f9861678c7ead8d68c
svv <vvsh1978@mail.ru>
2023-05-31 21:01:54 +0500

    feat: transfer todos array to parent component(ToDoListPage)

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -2,54 +2,34 @@ import { ToDo } from '../../models/todo-item'
 import './ToDoList.scss'
 import { ToDoListItem } from "./ToDoListItem/ToDoListItem"
 
-export const ToDoList = () => {
+export const ToDoList = (props: { todos: ToDo[] }) => {
 
-  const todos: ToDo[] = [
-    {
-      id: 0,
-      text: 'Первая задача',
-      isDone: false
-    },
-    {
-      id: 1,
-      text: 'Вторая задача',
-      isDone: true
-    },
-    {
-      id: 2,
-      text: 'Третья задача',
-      isDone: true
-    },
-    {
-      id: 3,
-      text: 'Четвертая задача',
-      isDone: false
-    }
-  ]
+  const checkedList = () => {
+    return props.todos
+      .filter((item) => item.isDone)
+      .map((item, idx) => {
+        return (
+          <ToDoListItem toDoItem={item} key={idx} />
+        )
+      })
+  }
+  const unCheckedList = () => {
+    return props.todos
+      .filter((item) => !item.isDone)
+      .map((item, idx) => {
+        return (
+          <ToDoListItem toDoItem={item} key={idx} />
+        )
+      })
+  }
 
   return (
     <div className="todo-container">
       <ul className="todo-list failed">
-        {
-          todos
-            .filter((item) => !item.isDone)
-            .map((item, idx) => {
-              return (
-                <ToDoListItem toDoItem={item} key={idx} />
-              )
-            })
-        }
+        {unCheckedList()}
       </ul>
       <ul className="todo-list completed">
-        {
-          todos
-            .filter((item) => item.isDone)
-            .map((item, idx) => {
-              return (
-                <ToDoListItem toDoItem={item} key={idx} />
-              )
-            })
-        }
+        {checkedList()}
       </ul>
     </div>
   )
--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -3,13 +3,38 @@
 import { Form } from "../components/Form/Form"
 import { Header } from "../components/Header/Header"
 import { ToDoList } from "../components/ToDoList/ToDoList"
+import { ToDo } from "../models/todo-item"
 
 export const ToDoListPage = () => {
+
+    const todos: ToDo[] = [
+        {
+            id: 0,
+            text: 'Первая задача',
+            isDone: false
+        },
+        {
+            id: 1,
+            text: 'Вторая задача',
+            isDone: true
+        },
+        {
+            id: 2,
+            text: 'Третья задача',
+            isDone: true
+        },
+        {
+            id: 3,
+            text: 'Четвертая задача',
+            isDone: false
+        }
+    ]
+
     return (
         <>
             <Header />
             <Form />
-            <ToDoList />
+            <ToDoList todos={todos} />
         </>
     )
 }
