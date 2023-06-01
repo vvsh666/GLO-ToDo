9717bf406b0a2188ad750d2f8ab136a13f5c01e9
svv <vvsh1978@mail.ru>
2023-06-01 20:49:17 +0500

    feat: change Link to NavLink, tracking and set active class

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import classes from './Header.module.scss'
 
 export const Header = () => {
+  const getActiveClass = ({ isActive }: { isActive: boolean }): string => {
+    return isActive ? `${classes.active} ${classes.link}` : classes.link
+  }
+
   return (
     <header className={classes.header}>
       <div className={classes.container}>
-        <Link to='/' className={classes.link}>Home</Link>
-        <Link to='/todo' className={classes.link}>ToDo</Link>
+        <NavLink to='/' className={getActiveClass} >Home</NavLink>
+        <NavLink to='/todo' className={getActiveClass} >ToDo</NavLink>
       </div>
     </header>
   )
