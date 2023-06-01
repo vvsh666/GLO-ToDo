8e9bc87a5120c340274c9a5665af31b6bc48e167
svv <vvsh1978@mail.ru>
2023-06-01 15:29:51 +0500

    feat: install and set react-router

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './assets/scss/normalize.scss';
 import './assets/scss/style.scss';
 import { ToDoListPage } from './pages/ToDoListPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoListPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>         
+        <Route path='/todo' element={<ToDoListPage />}></Route>
+      </Routes>      
+    </BrowserRouter>    
   </React.StrictMode>
 );
